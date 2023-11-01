c9473bdf299e1dfe0d27df4d2d03e9cde00a2ad1
Rehan904 <rehan904@yahoo.com>
2023-10-31 20:26:12 -0400

    added backend address

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5001"
-    : "https://vehicleinventorybackend.onrender.com/";
+    : "https://vehicleinventorybackend.onrender.com";
 export const VEHICALS_URL = "/api/vehical";
